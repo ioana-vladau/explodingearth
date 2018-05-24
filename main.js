b164345e9b2af3ae615aa59929ec93ff37caaa30
Ioana Vladau <ioan0414@stud.kea.dk>
2018-05-24 13:15:34 +0200

    exploding earth

--- main.js
+++ main.js
@@ -15,5 +15,13 @@
  loadSVG("assets/sarah.svg", "div", function(){
      console.log("sarah loaded");
      document.querySelector("#booms").style.display="none";
+     let stuff = document.querySelectorAll(".land path, .water path");
+     console.log(stuff)
+     stuff.forEach((el,i)=>{
+         console.log(i)
+         setTimeout(()=>{
+            el.style.transform = `translate(${Math.random()*100-50}px, ${Math.random()*100-50}px)`;
+        }, i*5)
+    })
      
  });
